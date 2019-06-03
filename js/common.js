861361d8c6c9c224da6efc049f63f8fad044a3d9
Vlad Donchenko <2pi14b.donchenko@gmail.com>
2019-06-03 16:20:51 +0300

    Исправлена ошибка Cannot read property 'classList' of undefined

--- js/common.js
+++ js/common.js
@@ -1,83 +1,103 @@
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Функция увеличивает индекс на 1, показывает следующй слайд
-function plusSlide() {
-    showSlides(slideIndex += 1);
+
+
+
+if(document.querySelectorAll(".products-slider-wrapper-item").length > 0){
+
+    var slideIndex = 1;
+
+    // Функция увеличивает индекс на 1, показывает следующй слайд
+    function plusSlide() {
+        showSlides(slideIndex += 1);
+    }
+
+    // Функция уменьшяет индекс на 1, показывает предыдущий слайд
+    function minusSlide() {
+        showSlides(slideIndex -= 1);  
+    }
+
+    // Устанавливает текущий слайд
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    showSlides(slideIndex);
+    // Основная функция слайдера 
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("products-slider-wrapper-item");
+        var dots = document.getElementsByClassName("products-slider-dots-item");
+
+
+        
+        if (n > slides.length) {
+          slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].classList.add("hidden-slide");
+
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" products-slider-dots-active", "");
+        }
+        slides[slideIndex - 1].classList.remove("hidden-slide");
+        dots[slideIndex - 1].className += " products-slider-dots-active";
+    }
+
+
+    var slideIndex_info = 1;
+
+    showSlides__info(slideIndex_info);
+
+    // Функция увеличивает индекс на 1, показывает следующй слайд
+    function plusSlide__info() {
+        showSlides__info(slideIndex_info += 1);
+    }
+
+    // Функция уменьшяет индекс на 1, показывает предыдущий слайд
+    function minusSlide__info() {
+        showSlides__info(slideIndex_info -= 1);  
+    }
+
+    // Устанавливает текущий слайд
+    function currentSlide__info(n) {
+       showSlides__info(slideIndex_info = n);
+    }
+
+    // Основная функция слайдера 
+    function showSlides__info(n) {
+        var i;
+        var slides = document.getElementsByClassName("information-slider-item");
+        var dots = document.getElementsByClassName("information-slider-dots-item");
+        
+        if (n > slides.length) {
+            slideIndex_info = 1;
+        }
+        if (n < 1) {
+            slideIndex_info = slides.length;
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" information-slider-dots-active", "");
+        }
+        slides[slideIndex_info - 1].style.display = "flex";
+        dots[slideIndex_info - 1].className += " information-slider-dots-active";
+    }
+
 }
 
-// Функция уменьшяет индекс на 1, показывает предыдущий слайд
-function minusSlide() {
-    showSlides(slideIndex -= 1);  
-}
 
-// Устанавливает текущий слайд
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
 
-// Основная функция слайдера 
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("products-slider-wrapper-item");
-    var dots = document.getElementsByClassName("products-slider-dots-item");
-    
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
 
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" products-slider-dots-active", "");
-    }
-    slides[slideIndex - 1].style.display = "flex";
-    dots[slideIndex - 1].className += " products-slider-dots-active";
-}
 
-var slideIndex_info = 1;
 
-showSlides__info(slideIndex_info);
 
-// Функция увеличивает индекс на 1, показывает следующй слайд
-function plusSlide__info() {
-    showSlides__info(slideIndex_info += 1);
-}
 
-// Функция уменьшяет индекс на 1, показывает предыдущий слайд
-function minusSlide__info() {
-    showSlides__info(slideIndex_info -= 1);  
-}
-
-// Устанавливает текущий слайд
-function currentSlide__info(n) {
-   showSlides__info(slideIndex_info = n);
-}
-
-// Основная функция слайдера 
-function showSlides__info(n) {
-    var i;
-    var slides = document.getElementsByClassName("information-slider-item");
-    var dots = document.getElementsByClassName("information-slider-dots-item");
-    
-    if (n > slides.length) {
-        slideIndex_info = 1;
-    }
-    if (n < 1) {
-        slideIndex_info = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" information-slider-dots-active", "");
-    }
-    slides[slideIndex_info - 1].style.display = "flex";
-    dots[slideIndex_info - 1].className += " information-slider-dots-active";
-}
 
