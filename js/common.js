8507e541b7a8ff02c90241475da76c984c470279
Vlad Donchenko <2pi14b.donchenko@gmail.com>
2019-06-09 13:22:50 +0300

    Валидация формы и открытие модалки

--- js/common.js
+++ js/common.js
@@ -1,7 +1,3 @@
-
-
-
-
 if(document.querySelectorAll(".products-slider-wrapper-item").length > 0){
 
     var slideIndex = 1;
@@ -27,8 +23,6 @@ if(document.querySelectorAll(".products-slider-wrapper-item").length > 0){
         var i;
         var slides = document.getElementsByClassName("products-slider-wrapper-item");
         var dots = document.getElementsByClassName("products-slider-dots-item");
-
-
         
         if (n > slides.length) {
           slideIndex = 1;
@@ -92,9 +86,87 @@ if(document.querySelectorAll(".products-slider-wrapper-item").length > 0){
 
 }
 
+/*--modal open*/
+var ModalAction = function(openModalClassName, modalClassName, closeModalName) {
+
+    var openModalClassName = document.querySelector(openModalClassName);
+    var modalClassName = document.querySelector(modalClassName);
+    var closeModalName = document.querySelector(closeModalName);
+
+    openModalClassName.addEventListener("click", function(evt){
+        evt.preventDefault();
+        modalClassName.classList.add("modal-show");
+
+          if (storageName && !formEmail) {
+             formName.value = storageName;
+             formEmail.focus();
+          }else if(storageEmail && !storageName){
+             formEmail.value = storageEmail;
+             formName.focus();
+          }else if(storageName && storageEmail){
+            formName.value = storageName;
+            formEmail.value = storageEmail;
+            mailText.focus();
+          }else{
+            formName.focus();
+          }
+      
+    });
+
+    closeModalName.addEventListener("click", function(evt){
+        evt.preventDefault();
+        modalClassName.classList.remove("modal-show");
+        modalClassName.classList.remove("modal-error");
+    });
+
+    window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+          evt.preventDefault();
+          if (modalClassName.classList.contains("modal-show")) {
+              modalClassName.classList.remove("modal-show");
+              modalClassName.classList.remove("modal-error");
+          }
+        }
+    });
+}
+
+ModalAction(".open-modal", ".write-us-modal", ".write-us-close");
+ModalAction(".open-map", ".modal-map", ".map-close");
 
 
+/*---form validation--*/
+var formModal = document.querySelector(".write-us-modal");
+var form = document.querySelector(".write-us-modal-form");
+var formName = document.getElementById("full-name");
+var formEmail = document.getElementById("email");
+var mailText = document.getElementById("mail-text");
 
+
+var isStorageSupport = true;
+var storageName = "";
+var storageEmail = "";
+
+  try {
+    storageName = localStorage.getItem("formName");
+    storageEmail = localStorage.getItem("formEmail");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  form.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    if(!formName.value || !formEmail.value){
+      evt.preventDefault();
+      formModal.classList.remove("modal-error");
+      formModal.offsetWidth = formModal.offsetWidth;
+      formModal.classList.add("modal-error");
+    }else{
+      if (isStorageSupport) {
+          localStorage.setItem("formName", formName.value);
+          localStorage.setItem("formEmail", formEmail.value);
+      }
+    }
+  });
 
 
 
