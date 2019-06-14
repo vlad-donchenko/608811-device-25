491915d42682160aa2cfc677400df5f9d4df3ce4
Vlad Donchenko <2pi14b.donchenko@gmail.com>
2019-06-14 22:41:35 +0300

    Правки после Защиты 1

--- js/common.js
+++ js/common.js
@@ -1,31 +1,27 @@
-if(document.querySelectorAll(".products-slider-wrapper-item").length > 0){
+if (document.querySelectorAll(".products-slider-wrapper-item").length > 0) {
 
     var slideIndex = 1;
 
-    // Функция увеличивает индекс на 1, показывает следующй слайд
     function plusSlide() {
         showSlides(slideIndex += 1);
     }
 
-    // Функция уменьшяет индекс на 1, показывает предыдущий слайд
     function minusSlide() {
-        showSlides(slideIndex -= 1);  
+        showSlides(slideIndex -= 1);
     }
 
-    // Устанавливает текущий слайд
     function currentSlide(n) {
         showSlides(slideIndex = n);
     }
-
     showSlides(slideIndex);
-    // Основная функция слайдера 
+
     function showSlides(n) {
         var i;
         var slides = document.getElementsByClassName("products-slider-wrapper-item");
         var dots = document.getElementsByClassName("products-slider-dots-item");
-        
+
         if (n > slides.length) {
-          slideIndex = 1;
+            slideIndex = 1;
         }
         if (n < 1) {
             slideIndex = slides.length
@@ -54,20 +50,20 @@ if(document.querySelectorAll(".products-slider-wrapper-item").length > 0){
 
     // Функция уменьшяет индекс на 1, показывает предыдущий слайд
     function minusSlide__info() {
-        showSlides__info(slideIndex_info -= 1);  
+        showSlides__info(slideIndex_info -= 1);
     }
 
     // Устанавливает текущий слайд
     function currentSlide__info(n) {
-       showSlides__info(slideIndex_info = n);
+        showSlides__info(slideIndex_info = n);
     }
 
-    // Основная функция слайдера 
+    // Основная функция слайдера
     function showSlides__info(n) {
         var i;
         var slides = document.getElementsByClassName("information-slider-item");
         var dots = document.getElementsByClassName("information-slider-dots-item");
-        
+
         if (n > slides.length) {
             slideIndex_info = 1;
         }
@@ -86,34 +82,33 @@ if(document.querySelectorAll(".products-slider-wrapper-item").length > 0){
 
 }
 
-/*--modal open*/
-var ModalAction = function(openModalClassName, modalClassName, closeModalName) {
+var ModalAction = function (openModalClassName, modalClassName, closeModalName) {
 
     var openModalClassName = document.querySelector(openModalClassName);
     var modalClassName = document.querySelector(modalClassName);
     var closeModalName = document.querySelector(closeModalName);
 
-    openModalClassName.addEventListener("click", function(evt){
+    openModalClassName.addEventListener("click", function (evt) {
         evt.preventDefault();
         modalClassName.classList.add("modal-show");
 
-          if (storageName && !formEmail) {
-             formName.value = storageName;
-             formEmail.focus();
-          }else if(storageEmail && !storageName){
-             formEmail.value = storageEmail;
-             formName.focus();
-          }else if(storageName && storageEmail){
+        if (storageName && !formEmail) {
+            formName.value = storageName;
+            formEmail.focus();
+        } else if (storageEmail && !storageName) {
+            formEmail.value = storageEmail;
+            formName.focus();
+        } else if (storageName && storageEmail) {
             formName.value = storageName;
             formEmail.value = storageEmail;
             mailText.focus();
-          }else{
+        } else {
             formName.focus();
-          }
-      
+        }
+
     });
 
-    closeModalName.addEventListener("click", function(evt){
+    closeModalName.addEventListener("click", function (evt) {
         evt.preventDefault();
         modalClassName.classList.remove("modal-show");
         modalClassName.classList.remove("modal-error");
@@ -121,11 +116,11 @@ var ModalAction = function(openModalClassName, modalClassName, closeModalName) {
 
     window.addEventListener("keydown", function (evt) {
         if (evt.keyCode === 27) {
-          evt.preventDefault();
-          if (modalClassName.classList.contains("modal-show")) {
-              modalClassName.classList.remove("modal-show");
-              modalClassName.classList.remove("modal-error");
-          }
+            evt.preventDefault();
+            if (modalClassName.classList.contains("modal-show")) {
+                modalClassName.classList.remove("modal-show");
+                modalClassName.classList.remove("modal-error");
+            }
         }
     });
 }
@@ -133,37 +128,32 @@ var ModalAction = function(openModalClassName, modalClassName, closeModalName) {
 ModalAction(".open-modal", ".write-us-modal", ".write-us-close");
 ModalAction(".open-map", ".modal-map", ".map-close");
 
-
-/*---form validation--*/
 var formModal = document.querySelector(".write-us-modal");
 var form = document.querySelector(".write-us-modal-form");
 var formName = document.getElementById("full-name");
 var formEmail = document.getElementById("email");
 var mailText = document.getElementById("mail-text");
-
-
 var isStorageSupport = true;
 var storageName = "";
 var storageEmail = "";
 
-  try {
+try {
     storageName = localStorage.getItem("formName");
     storageEmail = localStorage.getItem("formEmail");
-  } catch (err) {
+} catch (err) {
     isStorageSupport = false;
-  }
+}
 
-  form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    if(!formName.value || !formEmail.value){
-      evt.preventDefault();
-      formModal.classList.remove("modal-error");
-      formModal.offsetWidth = formModal.offsetWidth;
-      formModal.classList.add("modal-error");
-    }else{
-      if (isStorageSupport) {
-          localStorage.setItem("formName", formName.value);
-          localStorage.setItem("formEmail", formEmail.value);
-      }
+form.addEventListener("submit", function (evt) {
+    if (!formName.value || !formEmail.value) {
+        evt.preventDefault();
+        formModal.classList.remove("modal-error");
+        formModal.offsetWidth = formModal.offsetWidth;
+        formModal.classList.add("modal-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("formName", formName.value);
+            localStorage.setItem("formEmail", formEmail.value);
+        }
     }
-  });
+});
